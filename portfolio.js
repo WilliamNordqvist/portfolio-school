eac80f4f9a4507babf5149f1bbce506080dfbe17
willmister <wnordqvist@gmail.com>
2018-10-08 16:23:43 +0200

    added skill bar

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,32 @@ var burgerNav = document.querySelectorAll(".burgerNav");
             } 
         }
     });
+
+    // skill bar js
+
+    var php = function(){
+        document.querySelector(".php").className = "php1";
+    };
+    var css = function(){
+        document.querySelector(".css").className = "css1";
+    };
+    var htm = function(){
+    document.querySelector(".htm").className = "htm1";	
+    };
+    var jav = function(){
+    document.querySelector(".javascript").className = "javascript1";	
+    };
+    var ux = function(){
+        document.querySelector(".ux").className = "ux1";	
+    };
+    var sql = function(){
+        document.querySelector(".sql").className = "sql1";	
+    };
+
+    setTimeout(css,1000);
+    setTimeout(htm,1000);
+    setTimeout(jav,1000);
+    setTimeout(php,1000);
+    setTimeout(ux,1000);
+    setTimeout(sql,1000);
+
